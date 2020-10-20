9dfd4b5cf84c2ad03f36420b49978e7f06f9f22d
qsqnk <st087201@student.spbu.ru>
2020-10-20 22:09:03 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,11 +7,9 @@ extern void bubble_sort(long long int* array_of_numbers, int array_length);
 
 bool check_for_same_arguments(char* first_argument, char* second_argument) {
     if (strlen(first_argument) >= strlen("--to=") && strlen(second_argument) >= strlen("--to=")) {
-        //проверка на --to=
         if (strncmp(first_argument, second_argument, 5) == 0) {
             return 1;
         }
-        //проверка на --from=
         if (strncmp(first_argument, second_argument, 7) == 0) {
             return 1;
         }
@@ -60,18 +58,21 @@ char* get_type_of_argument(char* argument) {
     return "--from=";
 }
 
-void scan_arguments(int count_of_arguments, char** arguments, long long int* from, long long int* to) {
-    long long int temp_from = 0;
-    long long int temp_to = 0;
+void scan_arguments(int count_of_arguments, char** arguments,
+                    long long int* from, long long int* to, bool* is_from, bool* is_to) {
+    long long int temp_from = -1;
+    long long int temp_to = -1;
     for (int i = 0; i < count_of_arguments; i++) {
         if (is_argument_correct(arguments[i])) {
             if (strcmp(get_type_of_argument(arguments[i]), "--from=") == 0) {
+                *is_from = 1;
                 //+7 = +strlen(--from=)
                 if (strtoll(arguments[i] + 7, NULL, 10)) {
                     temp_from = strtoll(arguments[i] + 7, NULL, 10);
                 }
             }
             if (strcmp(get_type_of_argument(arguments[i]), "--to=") == 0) {
+                *is_to = 1;
                 //+5 = +strlen(--to=)
                 if (strtoll(arguments[i] + 5, NULL, 10)) {
                     temp_to = strtoll(arguments[i] + 5, NULL, 10);
@@ -84,40 +85,24 @@ void scan_arguments(int count_of_arguments, char** arguments, long long int* fro
     *to = temp_to;
 }
 
-void read_array(long long int* array, int* count_of_elements) {
+void read_array(long long int* array, int* count_of_elements, long long int from,
+                long long int to, bool is_from, bool is_to) {
     char c;
-    int i = 0;
-    scanf("%lli%c", &array[i], &c);
-    while (c != '\n') {
-        i++;
-        scanf("%lli%c", &array[i], &c);
-    }
-    *count_of_elements = i + 1;
-}
-
-void distribute_numbers(long long int* array_of_numbers, long long int* small_elements, long long int* normal_elements,
-                        int count_of_elements, long long int* big_elements, long long int from, long long int to, int* big_count,
-                        int* small_count, int* normal_count) {
-    int count_small = 0;
-    int count_big = 0;
-    int count_normal = 0;
-    for (int i = 0; i < count_of_elements; i++) {
-        if (array_of_numbers[i] <= from) {
-            small_elements[count_small] = array_of_numbers[i];
-            count_small++;
+    long long int temp_number = 0;
+    *count_of_elements = 0;
+    do {
+        scanf("%lli%c", &temp_number, &c);
+        if (temp_number <= from && is_from) {
+            fprintf(stdout, "%lli ", temp_number);
         }
-        if (array_of_numbers[i] >= to && to != 0) {
-            big_elements[count_big] = array_of_numbers[i];
-            count_big++;
+        if (temp_number >= to && is_to) {
+            fprintf(stderr, "%lli ", temp_number);
         }
-        if (array_of_numbers[i] > from && (array_of_numbers[i] < to || to == 0)) {
-            normal_elements[count_normal] = array_of_numbers[i];
-            count_normal++;
+        if ((!is_from || temp_number > from) && (!is_to || temp_number < to)) {
+            array[*count_of_elements] = temp_number;
+            *count_of_elements = *count_of_elements + 1;
         }
-    }
-    *small_count = count_small;
-    *big_count = count_big;
-    *normal_count = count_normal;
+    } while (c != '\n');
 }
 
 void copy_array(long long int* copy_from, long long int* copy_to, int count_of_elements) {
@@ -126,49 +111,32 @@ void copy_array(long long int* copy_from, long long int* copy_to, int count_of_e
     }
 }
 
-void numbers_output(long long int* small_elements, int count_small, long long int* big_elements, int count_big) {
-    for (int i = 0; i < count_small; i++) {
-        fprintf(stdout, "%lli ", small_elements[i]);
+int count_changed_elements(long long int* first_array, long long int* second_array, int array_length) {
+    int result = 0;
+    for (int i = 0; i < array_length; i++) {
+        if (first_array[i] != second_array[i]) {
+            result++;
+        }
     }
-    for (int i = 0; i < count_big; i++) {
-        fprintf(stderr, "%lli ", big_elements[i]);
-    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
     long long int from = 0;
     long long int to = 0;
     long long int* array_of_numbers;
-    long long int* small_elements;
-    long long int* big_elements;
-    long long int* normal_elements;
-    long long int* normal_elements_sorted;
+    long long int* array_of_sorted_numbers;
+    bool is_from = 0;
+    bool is_to = 0;
     array_of_numbers = (long long int*)malloc(100 * sizeof(long long int));
-    small_elements = (long long int*)malloc(100 * sizeof(long long int));
-    big_elements = (long long int*)malloc(100 * sizeof(long long int));
-    normal_elements = (long long int*)malloc(100 * sizeof(long long int));
-    normal_elements_sorted = (long long int*)malloc(100 * sizeof(long long int));
+    array_of_sorted_numbers = (long long int*)malloc(100 * sizeof(long long int));
     int count_of_elements = 0;
-    int count_of_small_elements = 0;
-    int count_of_big_elements = 0;
-    int count_of_normal_elements = 0;
-    //count of elements changed position after sorting
-    int count_of_changed_elements = 0;
     if (check_arguments(argc, argv)) {
         return check_arguments(argc, argv);
     }
-    scan_arguments(argc, argv, &from, &to);
-    read_array(array_of_numbers, &count_of_elements);
-    distribute_numbers(array_of_numbers, small_elements, normal_elements, count_of_elements, big_elements,
-                       from, to, &count_of_big_elements, &count_of_small_elements, &count_of_normal_elements);
-    copy_array(normal_elements, normal_elements_sorted, count_of_normal_elements);
-    bubble_sort(normal_elements_sorted, count_of_normal_elements);
-    //counting of elements changed position after sorting
-    for (int i = 0; i < count_of_normal_elements; i++) {
-        if (normal_elements[i] != normal_elements_sorted[i]) {
-            count_of_changed_elements++;
-        }
-    }
-    numbers_output(small_elements, count_of_small_elements, big_elements, count_of_big_elements);
-    return count_of_changed_elements;
+    scan_arguments(argc, argv, &from, &to, &is_from, &is_to);
+    read_array(array_of_numbers, &count_of_elements, from, to, is_from, is_to);
+    copy_array(array_of_numbers, array_of_sorted_numbers, count_of_elements);
+    bubble_sort(array_of_sorted_numbers, count_of_elements);
+    return count_changed_elements(array_of_numbers, array_of_sorted_numbers, count_of_elements);
 }
